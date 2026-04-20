ee059a103ef0a21c9b15e2fb4110712d81218871
yukan05 <shesheasdf159@gmail.com>
2026-04-20 13:52:56 +0800

    Update 12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -1,7 +1,7 @@
 <?php
-    session_start();
-    unset($_SESSION["id"]);
+    session_start();//啟動 Session。
+    unset($_SESSION["id"]);//用來刪除變數，也就是登出
     echo "登出成功....";
-    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";//3秒後連結到2.login.html也就是登入頁面）。
 
 ?>
