b743cae08389b8a8c2478e26eb714ade4bcbc2bd
yukan05 <shesheasdf159@gmail.com>
2026-05-11 15:38:08 +0800

    Add files via upload

---
+++ 17.user_delete.php
@@ -0,0 +1,19 @@
+<?php
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "請登入帳號";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{   
+        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
+        $sql="delete from user where id='{$_GET["id"]}'";
+        #echo $sql;
+        if (!mysqli_query($conn,$sql)){
+            echo "使用者刪除錯誤";
+        }else{
+            echo "使用者刪除成功";
+        }
+        echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
+    }
+?>
---
+++ 19.user_edit_form.php
@@ -0,0 +1,26 @@
+<html>
+    <head><title>修改使用者</title></head>
+    <body>
+    <?php
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "請登入帳號";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{   
+        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
+        $result=mysqli_query($conn, "select * from user where id='{$_GET['id']}'");
+        $row=mysqli_fetch_array($result);
+        echo "
+        <form method=post action=20.user_edit.php>
+            <input type=hidden name=id value={$row['id']}>
+            帳號：{$row['id']}<br> 
+            密碼：<input type=text name=pwd value={$row['pwd']}><p></p>
+            <input type=submit value=修改>
+        </form>
+        ";
+    }
+    ?>
+    </body>
+</html>
