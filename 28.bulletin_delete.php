ac962389a6f952facdd9f55866c893198ecbbde4
yukan05 <shesheasdf159@gmail.com>
2026-05-25 15:47:30 +0800

    Add files via upload

---
+++ 28.bulletin_delete.php
@@ -0,0 +1,19 @@
+<?php
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "請登入帳號";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{   
+        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
+        $sql="delete from bulletin where bid='{$_GET["bid"]}'";
+        #echo $sql;
+        if (!mysqli_query($conn,$sql)){
+            echo "佈告刪除錯誤";
+        }else{
+            echo "佈告刪除成功";
+        }
+        echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+    }
+?>
