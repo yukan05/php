f47e5e00046bf3a84fc51abeed0ec5fab095ee40
yukan05 <shesheasdf159@gmail.com>
2026-06-02 13:36:47 +0800

    Add files via upload

---
+++ 27.bulletin_edit.php
@@ -0,0 +1,20 @@
+<?php
+
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "請登入帳號";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{   
+        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
+        if (!mysqli_query($conn, "update bulletin set title='{$_POST['title']}',content='{$_POST['content']}',time='{$_POST['time']}',type={$_POST['type']} where bid='{$_POST['bid']}'")){
+            echo "修改錯誤";
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+        }else{
+            echo "修改成功，三秒鐘後回到佈告欄列表";
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+        }
+    }
+
+?>
