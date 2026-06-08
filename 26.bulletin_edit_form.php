4bdfc27cbba9968be4026e7d9a262f32f305d27e
yukan05 <shesheasdf159@gmail.com>
2026-06-08 18:08:51 +0800

    Update 26.bulletin_edit_form.php

--- 26.bulletin_edit_form.php
+++ 26.bulletin_edit_form.php
@@ -1,15 +1,16 @@
 <?php
-    error_reporting(0);
-    session_start();
+    error_reporting(0);// 關閉錯誤回報
+    session_start();// 啟動 Session
     if (!$_SESSION["id"]) {
         echo "please login first";
         echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
-    }
+    }//如果未登入，顯示請登入帳號並且3秒後回到登入畫面
     else{
         
-        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
-        $result=mysqli_query($conn, "select * from bulletin where bid={$_GET["bid"]}");
-        $row=mysqli_fetch_array($result);
+        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");//建立連結和登入資料庫
+        $result=mysqli_query($conn, "select * from bulletin where bid={$_GET["bid"]}");//執行SQL命令，從bulletin資料表找出GET傳來bid(佈告編號)的資料
+        $row=mysqli_fetch_array($result);//將查詢結果抓取出來，並以陣列形式存入 $row 中
+        
         $checked1="";
         $checked2="";
         $checked3="";
@@ -19,6 +20,7 @@
             $checked2="checked";
         if ($row['type']==3)
             $checked3="checked";
+        //用來判斷舊資料的佈告類型，先預設都回空白，再將對應的變數設為checked
         echo "
         <html>
             <head><title>修改佈告</title></head>
@@ -36,5 +38,7 @@
             </body>
         </html>
         ";
+        //輸出修改表單，佈告編號使用hidden隱藏欄位傳送；標題、內容與時間欄位皆預設帶入舊資料；單選按鈕則依上方判斷帶入checked狀態。
+        //按下修改佈告會將資料送出至 27.bulletin_edit.php，按下清除欄位回到舊資料。
     }
 ?>
