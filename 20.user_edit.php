2c4c39c9dbd25df5006f607107800cdd163a9bfe
yukan05 <shesheasdf159@gmail.com>
2026-06-08 16:55:08 +0800

    Update 20.user_edit.php

--- 20.user_edit.php
+++ 20.user_edit.php
@@ -5,16 +5,17 @@
     if (!$_SESSION["id"]) {
         echo "請登入帳號";
         echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
-    }
+    }//如果未登入，顯示請登入帳號並且3秒後回到登入畫面
     else{   
-        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
-        if (!mysqli_query($conn, "update user set pwd='{$_POST['pwd']}' where id='{$_POST['id']}'")){
+        $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");//建立連結和登入
+        if (!mysqli_query($conn, "update user set pwd='{$_POST['pwd']}' where id='{$_POST['id']}'")){//sql命令，更新user資料表裡post傳來的帳號，把密碼改成post傳來的密碼
             echo "修改錯誤";
             echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
-        }else{
+        }//如果失敗，顯示修改錯誤並且3秒後回到使用者管理畫面
+        else{
             echo "修改成功，三秒鐘後回到網頁";
             echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
-        }
+        }//如果成功，顯示修改成功，三秒鐘後回到網頁並且3秒後回到使用者管理畫面
     }
 
 ?>
