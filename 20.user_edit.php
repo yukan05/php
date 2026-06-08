80253ead2a706f4f4cdf7aa4fe6202fa3e695beb
yukan05 <shesheasdf159@gmail.com>
2026-06-08 16:56:17 +0800

    Update 20.user_edit.php

--- 20.user_edit.php
+++ 20.user_edit.php
@@ -8,7 +8,7 @@
     }//如果未登入，顯示請登入帳號並且3秒後回到登入畫面
     else{   
         $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");//建立連結和登入
-        if (!mysqli_query($conn, "update user set pwd='{$_POST['pwd']}' where id='{$_POST['id']}'")){//sql命令，更新user資料表裡post傳來的帳號，把密碼改成post傳來的密碼
+        if (!mysqli_query($conn, "update user set pwd='{$_POST['pwd']}' where id='{$_POST['id']}'")){//sql命令，更新user資料表裡post傳來帳號的密碼，改成post傳來的密碼
             echo "修改錯誤";
             echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
         }//如果失敗，顯示修改錯誤並且3秒後回到使用者管理畫面
